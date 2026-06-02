3616aed11b54a74ffde9b3ef5392f52ef17c2287
josecamilomunozr <munozroj@sheridancollege.ca>
2026-06-02 10:49:37 -0400

    JoseCamilo Munoz mod luckyCard

---
+++ src/cardprinciplessa/Card.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cardprinciplessa;
+
+/**
+ *
+ * @author pepecamilo
+ */
+public class Card {
+    
+    public enum Suit {HEARTS, SPADES, CLUBS,DIAMONDS}
+    public enum Value{ACE,TWO,THREE,FOUR,FIVE,SIX,SEVENEIGHT,NINE,TEN,JACK,QUEEN,KING,JOKER}
+    
+    private Value value;
+    private Suit suit;
+    
+    public Card(Suit s, Value v){
+        
+        this.suit=s;
+        this.value=v;
+        
+    }
+
+    /**
+     * @return the value
+     */
+    public Value getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the suit
+     */
+    public Suit getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+    
+}
---
+++ src/cardprinciplessa/CardPrinciplesSa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cardprinciplessa;
+
+/**
+ *
+ * @author pepecamilo
+ */
+public class CardPrinciplesSa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Card[] hand = GenerateCard.generate(10);
+        for(Card c : hand){
+            System.out.println(c.getSuit()+" "+c.getValue());
+        }
+    }
+    
+}
